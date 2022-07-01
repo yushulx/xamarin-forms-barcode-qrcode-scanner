efbb9aca170d2378bf5f99da297c10743093a25e
yushulx <lingxiao1002@gmail.com>
2022-07-01 16:38:35 +0800

    first commit

---
+++ CustomRenderer/ImageView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.IO;
+using System.Reflection;
+using System.Net.Http;
+
+namespace CustomRenderer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageView : ContentPage
+    {
+        string path;
+        SKBitmap bitmap;
+        public ImageView(string imagepath)
+        {
+            InitializeComponent();
+
+            try
+            {
+                using (var stream = new SKFileStream(imagepath))
+                {
+                    bitmap = SKBitmap.Decode(stream);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        //https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/graphics/skiasharp/bitmaps/displaying
+        async void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            float scale = Math.Min((float)info.Width / bitmap.Width,
+                               (float)info.Height / bitmap.Height);
+            float x = (info.Width - scale * bitmap.Width) / 2;
+            float y = (info.Height - scale * bitmap.Height) / 2;
+            SKRect destRect = new SKRect(x, y, x + scale * bitmap.Width,
+                                               y + scale * bitmap.Height);
+
+            canvas.DrawBitmap(bitmap, destRect);
+
+        }
+    }
+}
---
+++ CustomRenderer/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomRenderer
+{
+	public partial class MainPage : ContentPage
+	{
+        string PhotoPath = "";
+
+        public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+        //https://docs.microsoft.com/en-us/xamarin/essentials/media-picker?context=xamarin%2Fxamarin-forms&tabs=android
+		async void OnTakePhotoButtonClicked (object sender, EventArgs e)
+		{
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {PhotoPath}");
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                PhotoPath = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
+
+            await Navigation.PushAsync(new ImageView(PhotoPath));
+        }
+
+        async void OnTakeVideoButtonClicked(object sender, EventArgs e)
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+            if (status == PermissionStatus.Granted)
+            {
+                //We have permission!
+                await Navigation.PushAsync(new CameraPage());
+            }
+            else
+            {
+
+                //Ask for the permission
+                status = await Permissions.RequestAsync<Permissions.Camera>();
+                if (status == PermissionStatus.Granted)
+                {
+                    //YES, now we have permission
+                    //Let the user know why
+                    await DisplayAlert("Permission needed", "We've got permission", "Ok");
+                    await Navigation.PushAsync(new CameraPage());
+                }
+                else
+                {
+                    //Let the user know why
+                    await DisplayAlert("Permission needed", "I will need Camera permission for this action", "Ok");
+                }
+            }
+        }
+    }
+}
+
